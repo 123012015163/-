b70e28dcfa8a41e8be4cc1bc2b8c2982a73fd724
123012015163 <32355677+123012015163@users.noreply.github.com>
2017-11-22 20:56:57 +0800

    Task_FCFS_SJF

---
+++ TaskFCFSSJF/src/Task/SingleSJF.java
@@ -0,0 +1,93 @@
+package Task;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class SingleSJF extends TASK{
+	   public void singleSJF(){//短作业优先(单线程)
+	    	File file = new File("input.txt");//创建文件对象
+
+	        boolean [] Server;
+	        Server =new boolean[100];
+	        for(int i=0;i<100;i++){
+	        	Server[i]=false;
+	        }
+	    	if(file.exists()){//判断文件是否存在   	
+	    		try{   			
+	    		   FileInputStream in = new FileInputStream(file);   //创建FileInputStream对象，将数据信息从文件中读取出来
+	    		   Scanner scan =new Scanner(in);//用scan读取string信息(遇到空格,换行就自动换)
+/*	               for(int i=0;i<100;i++){//将文件夹里面的数据读取出来
+	            	   int a=in.read();
+	            	   Task_id[i]=a;
+	            	   int b=in.read();
+	            	   ArrivedTime[i] =b;
+	            	   int c=in.read();            	   
+	            	   ServerTime[i]=c;           	   
+	               }*/
+/*	               for(int i=0;i<100;i++){//将文件夹里面的数据读取出来
+	            	   System.out.println(Task_id[i]+" "+ArrivedTime[i]+" "+ServerTime[i]);
+	               }*/
+	    		   int i=0;
+	    		   while(scan.hasNext()){
+	    			   Task_id[i]=scan.nextInt();
+	    			   ArrivedTime[i]=scan.nextInt();
+	    			   ServerTime[i]=scan.nextInt();
+	    			   i=i+1;
+	    		   }
+	               scan.close();       	   
+	        	   int temp_id=0;//用来短暂存储任务ID 
+	        	   int first_unserver=temp_id;
+
+	        	   int time=0;
+	        	   System.out.println("TaskId ArrivalTime ServiceTime StartingTime FinishingTime TurnAroundTime WeightTurnAround");
+	        	   for(i=0;i<100;i++){
+	        		   for(int j=first_unserver;j<100;j++){
+	        			   if(Server[j]==false){//判断最靠前的任务是否被服务过
+	        				   temp_id=j;
+	        				   break;
+	        			   }
+	        		   }
+	        		   first_unserver=temp_id;
+	        		   for(int k=temp_id+1;k<100 && k<=time;k++){
+	        			   if(Server[k]==true){
+	        				   continue;
+	        			   }
+	        			   if(ServerTime[k]<ServerTime[temp_id] ){
+	        				   temp_id=k;	        				   
+	        			   }
+	        		   }
+	        		   if(ArrivedTime[temp_id]<=time){//如果任务达到时间小于前一个任务完成时间
+	        			   startingTime[temp_id]=time;
+	        			   finishingTime[temp_id]=time+ServerTime[temp_id];
+	        			   turnAroundTime[temp_id]=finishingTime[temp_id]-ArrivedTime[temp_id];
+	        			   weightTurnAround[temp_id]=turnAroundTime[temp_id]/ServerTime[temp_id];
+	        			   System.out.print(Task_id[temp_id]+"   ");
+	        			   System.out.print(ArrivedTime[temp_id]+"   ");
+	        			   System.out.print(ServerTime[temp_id]+"   ");
+	        			   System.out.print(startingTime[temp_id]+"   ");
+	        			   System.out.print(finishingTime[temp_id]+"   ");
+	        			   System.out.print(turnAroundTime[temp_id]+"   ");
+	        			   System.out.println( weightTurnAround[temp_id]);
+	        		   }else{
+	        			   startingTime[temp_id]=ArrivedTime[temp_id];
+	        			   finishingTime[temp_id]=ArrivedTime[temp_id]+ServerTime[temp_id];
+	        			   turnAroundTime[temp_id]=finishingTime[temp_id]-startingTime[temp_id];
+	        			   weightTurnAround[temp_id]=turnAroundTime[temp_id]/ServerTime[temp_id];
+	        			   System.out.print(Task_id[temp_id]+"   ");
+	        			   System.out.print(ArrivedTime[temp_id]+"   ");
+	        			   System.out.print(ServerTime[temp_id]+"   ");
+	        			   System.out.print(startingTime[temp_id]+"   ");
+	        			   System.out.print(finishingTime[temp_id]+"   ");
+	        			   System.out.print(turnAroundTime[temp_id]+"   ");
+	        			   System.out.println( weightTurnAround[temp_id]);
+	        		   }
+	        		   time=finishingTime[temp_id];
+	        		   Server[temp_id]=true;
+	        	   }
+	    		}catch(Exception e){
+	    			e.printStackTrace();//输出异常信息
+	    		}
+	    	}
+	    }
+}
---
+++ TaskFCFSSJF/src/Task/TASK.java
@@ -0,0 +1,65 @@
+package Task;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+public class TASK {
+    int TaskId[];//任务ID
+	int arrived_time[];//到达时间
+	int server_time[];//服务时间
+	int Task_id[] ;//定义变量
+	int []ServerTime;
+	int []ArrivedTime;
+	int startingTime[];//开始时间
+	int finishingTime[];//完成时间=开始时间+服务时间
+	int turnAroundTime[];//周转时间=完成时间-达到时间
+	float weightTurnAround[] ;//带权周转时间=周转时间/服务时间
+	
+	public TASK(){//构建函数
+		TaskId= new int[100];
+		arrived_time=new int[100];
+		server_time=new int[100];
+		Task_id=new int[100];//初始化变量任务ID
+		ServerTime=new int[100];
+		ArrivedTime=new int[100];
+		startingTime =new int[100];
+		finishingTime=new int[100];
+		turnAroundTime=new int[100];
+		weightTurnAround=new float[100];
+
+	}
+	/*这是最开始自己的数据读取来源*/
+    public void InputData(){  //将数据输入到文件里面 	 
+    	File file = new File("time.txt");//创建文件对象
+    	int[] numbers = {6,2,1,3,9};//在这个范围里面产生随机数
+    	if(file.exists()){//判断文件是否存在
+    		try{
+    			FileOutputStream out = new FileOutputStream(file);//创建FileOutputStream对象，将数据信息写入到文件中
+    			for(int i=0;i<100;i++){
+    				TaskId[i]=i+1;//赋予任务编号
+    				out.write(TaskId[i]);
+    				
+    				arrived_time[i]=i;//赋予任务达到时间
+    				out.write(arrived_time[i]);
+    				
+    	    		Random random = new Random();//利用随机数，来选择任务的服务时间
+    	    		int index = random.nextInt(numbers.length);
+    	    		server_time[i]=numbers[index];
+    	    		out.write(server_time[i]);   	    		
+    			}
+    			out.close();
+    		}catch(Exception e){
+    			e.printStackTrace();//输出异常信息
+    		}
+    	}else{//不存在的话，新建文件
+    		try{//try语句块捕捉可能出现的异常
+    			file.createNewFile();//创建该文件 
+    			InputData();
+    		}
+    		catch(Exception e){
+    			e.printStackTrace();//输出异常信息
+    		}
+    	}
+    }
+}
